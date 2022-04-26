f29ffa80a4400816056ce46da550d56963c71fa6
jennihan <40275488+jennihan@users.noreply.github.com>
2022-04-26 15:32:42 -0400

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,13 +1,3 @@
-provider "azurerm" {
-  features {}
-  
-  #client_id = "8a5a6080-7c09-454a-b7bc-133be0be0e6f"
-  #client_secret = "fY58Q~4tO8G4PW1DhnVet5a1gUNnpq9zKJa8Zdl3"
-  subscription_id = "4105419f-e724-4a4b-89f4-0cee686b07f1"
-  #tenant_id = "bc903af4-9e34-49de-ac79-1187f7413cfe"
-}
-
-
 terraform {
   required_providers {
     azurerm = {
